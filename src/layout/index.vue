3fcf9e516f9585cbc401ba62eb2d984edfaeaca0
CodeLeader <1144722582@qq.com>
2023-02-20 22:10:53 +0800

    记住密码功能实现(js-cookie+jsencrypt)

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,5 +1,6 @@
 <template>
   <el-button type="danger" @click="testHandler">测试接口</el-button>
+  <el-button type="danger" @click="testLogoutHandler">测试Logout接口</el-button>
 </template>
 
 <script setup>
@@ -8,6 +9,10 @@ import requestUtil from '@/util/request'
 
 const testHandler=async ()=>{
   let result=await requestUtil.get("test/user/list");
+}
+const testLogoutHandler=async ()=>{
+  let result=await requestUtil.get("/logout");
+
 
 }
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -27,7 +27,7 @@
 <!--          <template #prefix><svg-icon icon="password" /></template>-->
         </el-input>
       </el-form-item>
-      <el-checkbox  style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
+      <el-checkbox  v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
       <el-form-item style="width:100%;">
         <el-button
             size="large"
@@ -53,10 +53,13 @@ import store from '@/store'
 import qs from 'qs'
 import {ElMessage} from 'element-plus'
 import router from '@/router'
+import Cookies from "js-cookie";
+import { encrypt, decrypt } from "@/util/jsencrypt";
 const loginRef=ref(null)
 const loginForm=ref({
   username:"",
-  password:""
+  password:"",
+  rememberMe:false
 })
 
 const loginRules = {
@@ -67,6 +70,18 @@ const loginRules = {
 const handleLogin=()=>{
   loginRef.value.validate(async(valid)=>{
     if(valid){
+      // 勾选了需要记住密码设置在 cookie 中设置记住用户名和密码
+      if (loginForm.value.rememberMe) {
+        Cookies.set("username", loginForm.value.username, { expires: 30 });
+        Cookies.set("password", encrypt(loginForm.value.password), { expires: 30 });
+        Cookies.set("rememberMe", loginForm.value.rememberMe, { expires: 30 });
+      } else {
+        // 否则移除
+        Cookies.remove("username");
+        Cookies.remove("password");
+        Cookies.remove("rememberMe");
+      }
+
       let result=await requestUtil.post("login?"+qs.stringify(loginForm.value))
       let data=result.data
       if(data.code===200){
@@ -83,7 +98,18 @@ const handleLogin=()=>{
     }
   })
 }
+function getCookie() {
+  const username = Cookies.get("username");
+  const password = Cookies.get("password");
+  const rememberMe = Cookies.get("rememberMe");
+  loginForm.value = {
+    username: username === undefined ? loginForm.value.username : username,
+    password: password === undefined ? loginForm.value.password : decrypt(password),
+    rememberMe: rememberMe === undefined ? false : Boolean(rememberMe)
+  };
+}
 
+getCookie();
 </script>
 
 <style lang="scss" scoped>
