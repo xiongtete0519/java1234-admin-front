977c0df374363a85d20418184f2a2c643c9f4ee4
CodeLeader <1144722582@qq.com>
2023-02-22 19:50:01 +0800

    用户管理-删除和批量删除功能实现

--- src/views/sys/user/index.vue
+++ src/views/sys/user/index.vue
@@ -7,9 +7,14 @@
       </el-col>
       <el-button type="primary" :icon="Search" @click="initUserList">搜索</el-button>
       <el-button type="success" :icon="DocumentAdd" @click="handleDialogValue()">新增</el-button>
+      <el-popconfirm title="您确定批量删除这些记录吗？" @confirm="handleDelete(null)">
+        <template #reference>
+          <el-button type="danger" :disabled="delBtnStatus" :icon="Delete" >批量删除</el-button>
+        </template>
+      </el-popconfirm>
     </el-row>
 
-    <el-table :data="tableData" stripe style="width: 100%">
+    <el-table :data="tableData" stripe style="width: 100%" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" />
       <el-table-column prop="avatar" label="头像" width="80" align="center">
         <template v-slot="scope">
@@ -37,6 +42,11 @@
         <template v-slot="scope" >
           <el-button  type="primary" :icon="Tools" >分配角色</el-button>
           <el-button v-if="scope.row.username!=='admin'" type="primary" :icon="Edit" @click="handleDialogValue(scope.row.id)" />
+          <el-popconfirm v-if="scope.row.username!=='admin'" title="您确定要删除这条记录吗？" @confirm="handleDelete(scope.row.id)">
+            <template #reference>
+              <el-button  type="danger" :icon="Delete" />
+            </template>
+          </el-popconfirm>
         </template>
       </el-table-column>
     </el-table>
@@ -59,7 +69,7 @@ import { Search ,Delete,DocumentAdd ,Edit, Tools, RefreshRight} from '@element-p
 import requestUtil,{getServerUrl} from "@/util/request";
 import { ref } from 'vue'
 import Dialog from '@/views/sys/user/component/dialog'
-
+import { ElMessage, ElMessageBox } from 'element-plus'
 const tableData=ref([]);
 
 const total=ref(0)
@@ -70,11 +80,23 @@ const queryForm=ref({
   pageSize:10
 })
 
-//Dialog组件的三个值
+//Dialog组件传的三个值
 const dialogVisible=ref(false)
 const dialogTitle=ref("")
 const id=ref(-1)
 
+const delBtnStatus=ref(true)
+//复选框选中事件
+const multipleSelection=ref([])
+const handleSelectionChange=(selection)=>{
+  console.log("勾选了")
+  console.log(selection)
+  multipleSelection.value=selection;
+  delBtnStatus.value=selection.length===0;
+}
+
+
+//传的方法，子组件可以通过emits调用父组件事件
 const initUserList=async()=>{
   const res=await requestUtil.post("sys/user/list",queryForm.value);
   tableData.value=res.data.userList;
@@ -82,13 +104,13 @@ const initUserList=async()=>{
 }
 
 initUserList();
-
+//改变每页记录数触发
 const handleSizeChange=(pageSize)=>{
   queryForm.value.pageNum=1;
   queryForm.value.pageSize=pageSize;
   initUserList()
 }
-
+//改变当前页触发
 const handleCurrentChange=(pageNum)=>{
   queryForm.value.pageNum=pageNum;
   initUserList()
@@ -98,11 +120,35 @@ const handleDialogValue=(userId)=>{
   if(userId){
     id.value=userId;
     dialogTitle.value="用户修改"
-  }else{
+  }else{  //新增的时候没有传id
     id.value=-1;
     dialogTitle.value="用户添加"
   }
   dialogVisible.value=true
+}
+//批量删除
+const handleDelete=async (id)=>{
+  var ids = []
+  if(id){ //单行删除
+    ids.push(id)
+  }else{  //多行删除
+    multipleSelection.value.forEach(row=>{
+      ids.push(row.id)
+    })
+  }
+  const res=await requestUtil.post("sys/user/delete",ids)
+  if(res.data.code===200){
+    ElMessage({
+      type: 'success',
+      message: '执行成功!'
+    })
+    initUserList();
+  }else{
+    ElMessage({
+      type: 'error',
+      message: res.data.msg,
+    })
+  }
 }
 </script>
 
